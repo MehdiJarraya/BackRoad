73c36e0b331444a4780f39cef17c1998e29a79f2
MehdiJarraya <Mehdi.jarraya@gmail.com>
2020-05-31 16:09:54 +0100

    inital commit

---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+module.exports = {
+  siteMetadata: {
+    title: "BackRoads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say yes to adventure",
+    author: "@johndoe",
+    twitterUsername: "@john_smilga",
+    image: "/defaultBcg.jpeg",
+    siteUrl: "https://gatsby-backroad-project.netlify.com",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-transition-link`,
+    `gatsby-plugin-playground`,
+  ],
+}
